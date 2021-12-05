518f2536c8bd4fb5e9248560896cb796dde5ae90
dhkatz <dhkatz.16@gmail.com>
2021-12-04 20:27:24 -0800

    Split out map into scenes

--- packages/client/src/Game.js
+++ packages/client/src/Game.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Stars, Stats } from '@react-three/drei';
+import { OrbitControls, Stats } from '@react-three/drei';
 
-import { Particles } from './entities';
 import { Entity, Selection } from './components';
 import { useGameStore } from './stores';
+import { Space } from './scenes';
 
 export const Game = () => {
 	const entities = useGameStore((state) => state.entities);
@@ -14,14 +14,7 @@ export const Game = () => {
 	return (
 		<Canvas camera={{ position: [-20, 20, -20], fov: 75 }}>
 			<Stats />
-			<color attach={'background'} args={['#020209']} />
-			<fog attach="fog" args={['#070710', 100, 700]} />
-			<ambientLight intensity={0.5} />
-			<gridHelper args={[100, 10]}>
-				<lineBasicMaterial attach="material" color="#303030" />
-			</gridHelper>
-			<Stars />
-			<Particles />
+			<Space />
 			<Selection>
 				{entities.map(({ id, components, ...props }) => (
 					<Entity key={id} {...props}>
---
+++ packages/client/src/scenes/Space.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Stars } from '@react-three/drei';
+import { Particles } from '../entities';
+
+export const Space = () => {
+	return (
+		<>
+			<color attach={'background'} args={['#020209']} />
+			<fog attach="fog" args={['#070710', 100, 700]} />
+			<ambientLight intensity={0.5} />
+			<gridHelper args={[100, 10]}>
+				<lineBasicMaterial attach="material" color="#303030" />
+			</gridHelper>
+			<Stars />
+			<Particles />
+		</>
+	);
+};
